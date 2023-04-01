9ec0319a7b38b9083117bca718bd876face74619
Jack Phillips <jphillips@and.digital>
2023-04-01 12:47:55 +0100

    Navbar component added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Baths from './Pages/Baths';
 import Toilets from './Pages/Toilets';
+import Navigation from './Components/NavBar/NavBar';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/baths' element={<Baths/>} />
-        <Route path='/toilets' element={<Toilets />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Navigation />
+      <Container>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/baths' element={<Baths />} />
+            <Route path='/toilets' element={<Toilets />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </>
   );
 }
 
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Logo from '../../Assets/image.svg';
+import './NavBar.css'
+
+function Navigation() {
+  return (
+    <Navbar expand='lg'>
+      <Container>
+        <Navbar.Brand href='/'>
+          <img className='navbar-brand-img' src={Logo} alt='Logo' />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link href='/baths'>Baths</Nav.Link>
+            <Nav.Link href='/toilets'>Toilets</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
